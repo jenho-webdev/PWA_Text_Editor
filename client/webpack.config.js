af6ce38d2bbe120231d7f1a1e76d34eb34437c5b
Jen Ho <jen@jenho.website>
2023-08-16 23:50:45 -0700

    updated and fixed webpack.config.js on PWAmanifect file name and path issue. icon files name and output path updated as well.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,29 +1,79 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
+// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+// DONE: Add and configure workbox plugins for a service worker and manifest file.
+// DONE: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
+      publicPath: "/",
+    },
+    devServer: {
+      // The `hot` option is to use the webpack-dev-server in combination with the hot module replacement API.
+      hot: "only",
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "Webpack Plugin",
+      }),
+      // new MiniCssExtractPlugin(),
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "JATE",
+        short_name: "JATE",
+        description: "",
+        background_color: "#ffffff",
+        theme_color: "#000000",
+        crossorigin: "use-credentials", // or 'anonymous' if needed
+        icons: [
+          {
+            src: path.resolve("src/images/icon.png"), // Replace with your icon path
+            destination: path.join("assets", "icons"),
+            sizes: [16, 32, 96],
+          },
+        ],
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          //CSS loaders
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+        },
+        {
+          //babel loader
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
+          },
+        },
       ],
     },
   };
